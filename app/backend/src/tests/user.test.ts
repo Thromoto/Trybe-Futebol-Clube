b27121c525f431d86ad4244e9ea093b7d645a2e1
Thiago Omoto <thi.omoto@gmail.com>
2023-04-05 18:58:32 -0300

    test coverage

---
+++ app/backend/src/tests/user.test.ts
@@ -0,0 +1,46 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Users & Login', () => {
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
+
+  // let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Example, "findOne")
+  //     .resolves({
+  //       ...<Seu mock>
+  //     } as Example);
+  // });
+
+  // after(()=>{
+  //   (Example.findOne as sinon.SinonStub).restore();
+  // })
+
+  // it('...', async () => {
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      ...
+
+  //   expect(...)
+  // });
+
+  it('deve retornar um erro 400', async () => {
+    const httpResponse = await chai.request(app).post('/login').send({})
+    expect(httpResponse.status).to.equal(400);
+  })
+});
